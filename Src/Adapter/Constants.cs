a3479c84981e2ceeb5e34ddaf93a3c2bf20c403d
danielkzu <daniel@cazzulino.com>
2012-12-05 20:20:33 -0300

    Added state constants as a separate file, solves the source nuget problem of not having it because the GlobalAssemblyInfo.cs is not deployed.

--- Src/Adapter/Constants.cs
+++ Src/Adapter/Constants.cs
@@ -1,4 +1,4 @@
-#region BSD License
+﻿#region BSD License
 /* 
 Copyright (c) 2012, Clarius Consulting
 All rights reserved.
@@ -11,16 +11,23 @@ Redistribution and use in source and binary forms, with or without modification,
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 #endregion
-using System.Reflection;
 
-#if DEBUG
-[assembly: AssemblyConfiguration("DEBUG")]
-#endif
-#if RELEASE
-[assembly: AssemblyConfiguration("RELEASE")]
-#endif
-[assembly: AssemblyCompany("Clarius Consulting")]
-[assembly: AssemblyCopyright("Copyright © Clarius Consulting 2012")]
-
-[assembly: AssemblyVersion("1.0")]
-[assembly: AssemblyFileVersion("1.0.1212.0519")]
+namespace Patterns.Adapter
+{
+    /// <summary>
+    /// Shared constants across implementation and interface.
+    /// </summary>
+    internal static class Constants
+    {
+        /// <summary>
+        /// The identifier for the global state that lives in the AppDomain and provides 
+        /// the implementation for the extension method at runtime.
+        /// </summary>
+        public const string GlobalStateIdentifier = "{29938042-C16D-46BA-93D3-F513E88EC345}";
+        /// <summary>
+        /// The identifier for the transient state that lives in an AmbientSingleton that 
+        /// can be used in tests to replace the adapter service.
+        /// </summary>
+        public const string TransientStateIdenfier = "{E0A43955-6A73-4FD7-B30E-3FB699A804EC}";
+    }
+}
